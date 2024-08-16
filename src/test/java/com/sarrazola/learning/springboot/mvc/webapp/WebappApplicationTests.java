d24e91c5173de7a26326b50a603a3f5fc4f5f400
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-16 14:20:07 -0600

    Initialize project using Springboot Initializer.

---
+++ src/test/java/com/sarrazola/learning/springboot/mvc/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarrazola.learning.springboot.mvc.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
