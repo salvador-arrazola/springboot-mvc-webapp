45c9e10ddf253ad9955636968656bbec763af002
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-16 15:03:35 -0600

    Add a rest controller.

---
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
@@ -0,0 +1,22 @@
+package com.sarrazola.learning.springboot.mvc.webapp.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+  @GetMapping("/details2")
+  public Map<String, Object> details() {
+    Map<String, Object> responseBody = new HashMap<>();
+    responseBody.put("greeting", "Hello World Spring Boot !!!");
+    responseBody.put("name", "Guaripolo");
+    responseBody.put("lastname", "Arrazola");
+    responseBody.put("title", "Welcome Page");
+    return responseBody;
+  }
+
+}
