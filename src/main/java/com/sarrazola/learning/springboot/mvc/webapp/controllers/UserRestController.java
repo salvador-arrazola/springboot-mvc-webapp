08f4cc469774b258597f15bc2bc0f67c3fc572fc
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-16 15:22:29 -0600

    Add a request mapping.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
   @GetMapping("/details2")
@@ -18,5 +20,21 @@ public class UserRestController {
     responseBody.put("title", "Welcome Page");
     return responseBody;
   }
+
+// Same result by using Controller + ResponseBody:
+// @Controller
+// @RequestMapping("/api")
+// public class UserRestController {
+
+//   @GetMapping("/details2")
+//   @ResponseBody
+//   public Map<String, Object> details() {
+//     Map<String, Object> responseBody = new HashMap<>();
+//     responseBody.put("greeting", "Hello World Spring Boot !!!");
+//     responseBody.put("name", "Guaripolo");
+//     responseBody.put("lastname", "Arrazola");
+//     responseBody.put("title", "Welcome Page");
+//     return responseBody;
+//   }
 
 }
