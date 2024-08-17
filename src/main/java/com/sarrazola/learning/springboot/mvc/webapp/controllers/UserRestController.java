f43bcdf8d1663ade9b7c4f4d884634df68b4a5e2
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-16 20:35:59 -0600

    Add email to User.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
@@ -1,7 +1,6 @@
 package com.sarrazola.learning.springboot.mvc.webapp.controllers;
 
-import java.util.ArrayList;
-// import java.util.Arrays;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,6 @@ public class UserRestController {
   @GetMapping("/details")
   public UserDto details() {
 
-    // User user = new User("Guaripolo", "Arrazola");
     User user = new User();
     user.setName("Guaripolo");
     user.setLastname("Arrazola");
@@ -36,26 +34,16 @@ public class UserRestController {
     User user1 = new User();
     user1.setName("Guaripolo");
     user1.setLastname("Arrazola");
+
     User user2 = new User();
     user2.setName("Guachimingo");
     user2.setLastname("Torres");
+
     User user3 = new User();
     user3.setName("Patana");
     user3.setLastname("Murillo");
     
-    List<User> users = new ArrayList<>();
-    // users.add(new User("Guaripolo", "Arrazola"));
-    // users.add(new User("Guachimingo", "Torres"));
-    // users.add(new User("Patana", "Murillo"));
-    users.add(user1);
-    users.add(user2);
-    users.add(user3);
-
-    // Using Helper "Arrays":
-    // User user1 = new User("Guaripolo", "Arrazola");
-    // User user2 = new User("Guachimingo", "Torres");
-    // User user3 = new User("Patana", "Murillo");
-    // List<User> users = Arrays.asList(user1, user2, user3);
+    List<User> users = Arrays.asList(user1, user2, user3);
     
     return users;
   }
--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
@@ -6,17 +6,6 @@ public class User {
   private String lastname;
   private String email;
 
-  // public User(String name, String lastname) {
-  //   this.name = name;
-  //   this.lastname = lastname;
-  // }
-  
-  // public User(String name, String lastname, String email) {
-  //   this.name = name;
-  //   this.lastname = lastname;
-  //   this.email = email;
-  // }
-
   public String getName() {
     return name;
   }
