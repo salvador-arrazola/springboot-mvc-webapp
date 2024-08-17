f0662c1e1c3855e9f32f74ac722c77c26a9b3ea7
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-16 20:32:28 -0600

    Remove User constructors.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/UserRestController.java
@@ -17,19 +17,39 @@ public class UserRestController {
 
   @GetMapping("/details")
   public UserDto details() {
-    User user = new User("Guaripolo", "Arrazola");
+
+    // User user = new User("Guaripolo", "Arrazola");
+    User user = new User();
+    user.setName("Guaripolo");
+    user.setLastname("Arrazola");
+
     UserDto userDto = new UserDto();
     userDto.setGreeting("Hello World Spring Boot !!!");
     userDto.setUser(user);
+
     return userDto;
   }
 
   @GetMapping("/list")
   public List<User> list() {
+
+    User user1 = new User();
+    user1.setName("Guaripolo");
+    user1.setLastname("Arrazola");
+    User user2 = new User();
+    user2.setName("Guachimingo");
+    user2.setLastname("Torres");
+    User user3 = new User();
+    user3.setName("Patana");
+    user3.setLastname("Murillo");
+    
     List<User> users = new ArrayList<>();
-    users.add(new User("Guaripolo", "Arrazola"));
-    users.add(new User("Guachimingo", "Torres"));
-    users.add(new User("Patana", "Murillo"));
+    // users.add(new User("Guaripolo", "Arrazola"));
+    // users.add(new User("Guachimingo", "Torres"));
+    // users.add(new User("Patana", "Murillo"));
+    users.add(user1);
+    users.add(user2);
+    users.add(user3);
 
     // Using Helper "Arrays":
     // User user1 = new User("Guaripolo", "Arrazola");
--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
@@ -4,12 +4,19 @@ public class User {
 
   private String name;
   private String lastname;
+  private String email;
 
-  public User(String name, String lastname) {
-    this.name = name;
-    this.lastname = lastname;
-  }
+  // public User(String name, String lastname) {
+  //   this.name = name;
+  //   this.lastname = lastname;
+  // }
   
+  // public User(String name, String lastname, String email) {
+  //   this.name = name;
+  //   this.lastname = lastname;
+  //   this.email = email;
+  // }
+
   public String getName() {
     return name;
   }
@@ -24,6 +31,14 @@ public class User {
 
   public void setLastname(String lastname) {
     this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
   }
 
 }
