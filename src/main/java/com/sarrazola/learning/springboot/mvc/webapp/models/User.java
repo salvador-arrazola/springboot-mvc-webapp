ce98a13abc713e22c2acec7c2a3e17248257e726
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-17 21:23:11 -0600

    Add user constructors.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/models/User.java
@@ -6,6 +6,20 @@ public class User {
   private String lastname;
   private String email;
 
+  public User() {
+  }
+
+  public User(String name, String lastname) {
+    this.name = name;
+    this.lastname = lastname;
+  }
+
+  public User(String name, String lastname, String email) {
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
+  }
+
   public String getName() {
     return name;
   }
