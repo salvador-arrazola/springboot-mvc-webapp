73c2c270c6f5b8568dc2a6beeb094cc1665b71ff
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-08-19 18:19:53 -0600

    Fix imports.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/PathVariableController.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/PathVariableController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sarrazola.learning.springboot.mvc.webapp.models.dto.ParamDto;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
