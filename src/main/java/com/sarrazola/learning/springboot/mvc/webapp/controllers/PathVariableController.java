9ea92f8c5d339a3c6d1eeab07e75c315500bb999
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-10-21 15:09:33 -0600

    Fix imports and remove old comments.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/PathVariableController.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/controllers/PathVariableController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sarrazola.learning.springboot.mvc.webapp.models.User;
 import com.sarrazola.learning.springboot.mvc.webapp.models.dto.ParamDto;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/var")
@@ -34,20 +32,10 @@ public class PathVariableController {
 
   @GetMapping("/baz/{message}")
   public ParamDto baz(@PathVariable String message) {
-  // @PathVariable MUST have the same as defined in @RequestMapping.
-  // public ParamDto baz(@PathVariable String msg) {
-  // @PathVariables are required by default (required = true), but even if you change this option
-  // to false, the @PathVariable will still be required and you will get an error when calling the
-  // api with no value for the path variable:
-  // public ParamDto baz(@PathVariable(required = false) String message) {
       return new ParamDto(message);
-      // return new ParamDto(msg);
-  }
+    }
 
-  // REMINDER: Do not forget to put the value for path variables after the last slash (/) when
-  // opening the api URL.
-
-  @GetMapping("/mix/{product}/{id}")
+    @GetMapping("/mix/{product}/{id}")
   public Map<String, Object> mix(@PathVariable String product, @PathVariable Long id) {
     Map<String, Object> json = new HashMap<>();
     json.put("product", product);
