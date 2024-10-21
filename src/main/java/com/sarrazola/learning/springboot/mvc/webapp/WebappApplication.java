7c65319c3dc887aefa44e08d487fdb498495c0b3
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-10-21 16:03:55 -0600

    Add a custom configuration class.

--- src/main/java/com/sarrazola/learning/springboot/mvc/webapp/WebappApplication.java
+++ src/main/java/com/sarrazola/learning/springboot/mvc/webapp/WebappApplication.java
@@ -3,15 +3,8 @@ package com.sarrazola.learning.springboot.mvc.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
-// import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-// We can include multiple properties files, organized by modules or categories, inside the
-// "resources" folder:
-// @PropertySources({
-//   @PropertySource("classpath:values.properties"),
-//   @PropertySource("classpath:values2.properties")
-// })
 @PropertySource("classpath:values.properties")
 public class WebappApplication {
 
